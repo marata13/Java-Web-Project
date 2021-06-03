57355269054fe06fb93d42c85c6ff998603b1c6d
Konstantinos Argyriou <rounnus@gmail.com>
2021-06-03 14:18:15 +0300

    Set a few comments on query manager.

--- src/main/java/com/database/QueryManager.java
+++ src/main/java/com/database/QueryManager.java
@@ -6,6 +6,19 @@ import java.util.*;
 
 public class QueryManager {
 
+    /**
+     * Αυτη η βοηθητικη μεθοδος μας επιτρεπει να εκτελουμε
+     * αιτηματα προς την βαση, οτι αιτημα και να δωθει
+     * στην παραμετρο, θα το εκτελεσει. Αυτο αρκει να
+     * εχουμε δωσει το σωστο Connection και τις σωστες
+     * παραμετρους.
+     *
+     * @param query Το ερωτημα που προκειται να εκτελεστει.
+     * @param conn Η συνδεση προς την βαση.
+     * @param parameters Οι παραμετροι που θα τοποθετιθουν στο ερωτημα της SQL.
+     * @return Το συνολο τον εποτελεσματων απο την βαση.
+     * @throws SQLException SQL.
+     */
     private static ResultSet queryExecutor(String query, Connection conn, String... parameters) throws SQLException {
         int index;
 
@@ -23,6 +36,16 @@ public class QueryManager {
         return preparedStatement.getResultSet();
     }
 
+    /**
+     * Σε αυτη την μεθοδο τοποθετουμε τα στοιχεια
+     * που εχουμε λαβει απο την βαση μεσα σε ενα
+     * HashMap.
+     *
+     * @param fromDB Το συνολο των δεδομενων που λαβαμε απο την βαση.
+     * @param dataToRetrieve Τα δεδομενα που θελουμε να τραβιξουμε.
+     * @return Το HashMap με τα δεδομενα.
+     * @throws SQLException SQL
+     */
     private static HashMap<String, String> retrieveData(ResultSet fromDB, List<String> dataToRetrieve) throws SQLException {
         HashMap<String, String> retrievedData = new HashMap<>();
         fromDB.next();
@@ -35,6 +58,18 @@ public class QueryManager {
     }
 
 
+    /**
+     * Σε αυτη την μεθοδο τραβαμε απο την βαση δεδομενων ολα
+     * τα απαραιτητα δεδομενα.
+     *
+     * @param username Το username με το οποιο θα ψαξουμε στην βαση.
+     * @param query Το ερωτημα που θελουμε να εκτελεσουμε.
+     * @param conn Η συνδεση που εχουμε αποκαταστησει.
+     * @param table Ο πινακας απο τον οποιο θελουμε να τραβιξουμε τα δεδομενα.
+     * @param retrieves Τα συγκεκριμενα δεδομενα που θελουμε να τραβιξουμε ( π.χ username και password )
+     * @return Ενα HashMap<String,String> το οποιο περιεχει τις πληροφοριες που ζητηθηκαν.
+     * @throws SQLException Αφου εμπλεκουμε SQL ερωτηματα.
+     */
     public static HashMap<String, String> getFromDatabase(String username, String query, Connection conn, String table, String... retrieves) throws SQLException {
         ResultSet resultsFromDB;
         resultsFromDB = QueryManager.queryExecutor(MessageFormat.format(query, table), conn, username);
